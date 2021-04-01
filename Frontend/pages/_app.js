d3c8e0122d28ac1611fc214791f2db0d9f3a986f
buncy <karthik138105@gmail.com>
2021-04-01 15:20:46 +0530

    patched up frontend

--- Frontend/pages/_app.js
+++ Frontend/pages/_app.js
@@ -1,5 +1,16 @@
 import "../styles/globals.css";
 import Page from "../components/Page";
+import Router from "next/router";
+import Nprogress from "nprogress";
+//TODO:Swap with our own
+//import "nprogress/nprogress.css";
+import "../components/styles/nprogress.css";
+
+Router.events.on("routeChangeStart", () => {
+  Nprogress.start();
+});
+Router.events.on("routeChangeComplete", () => Nprogress.done());
+Router.events.on("routeChangeError", () => Nprogress.done());
 
 function MyApp({ Component, pageProps }) {
   return (
