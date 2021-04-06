c24572fea7ae7947018171f47d2816cd55fc3056
buncy <karthik138105@gmail.com>
2021-04-06 15:43:28 +0530

    single product page styles

--- Frontend/components/SingleProduct/index.js
+++ Frontend/components/SingleProduct/index.js
@@ -1,6 +1,24 @@
 import { gql, useQuery } from "@apollo/client";
-
 import DisplayError from "../DisplayError";
+import Head from "next/head";
+import styled from "styled-components";
+
+const ProductStyles = styled.div`
+  display: grid;
+  grid-auto-columns: 1fr;
+  grid-auto-flow: column;
+
+  max-width: var(--maxWidth);
+  justify-content: center;
+  align-items: top;
+  gap: 2rem;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+`;
+
 const SINGLE_ITEM_QUERY = gql`
   query SINGLE_ITEM_QUERY($id: ID!) {
     Product(where: { id: $id }) {
@@ -28,7 +46,10 @@ export default function SingleProduct({ id }) {
   if (error) return <DisplayError error={error} />;
   const { Product } = data;
   return (
-    <div>
+    <ProductStyles>
+      <Head>
+        <title>Sickfits | {Product.name}</title>
+      </Head>
       <div className="details">
         <img
           src={Product.photo.image.publicUrlTransformed}
@@ -37,6 +58,6 @@ export default function SingleProduct({ id }) {
         <h2>{Product.name}</h2>
         <p>{Product.description}</p>
       </div>
-    </div>
+    </ProductStyles>
   );
 }
